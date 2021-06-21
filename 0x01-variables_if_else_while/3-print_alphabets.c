84a191359bdfdadd7f054f1fcdcd22399f27961f
adute <aduti.alemu@gmail.com>
2021-06-21 04:51:23 -0700

    My third , variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char s = 'a';
+	char c = 'A';
+
+	while (s <= 'z')
+	{
+		putchar(s);
+		s++;
+	}
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
