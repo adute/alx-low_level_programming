284ec0166f3b1671341ead47d8fcae7d9d147d5e
adute <aduti.alemu@gmail.com>
2021-06-22 13:13:12 -0700

    My functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char h [] = "Holberton";
-	int i=0;
+	char h[] = "Holberton";
+	unsigned char i = 0;
 
-	while (i <=9)
+	while (i < 9)
 	{
 		_putchar(h[i]);
-
+		i++;
 	}
 	_putchar('\n');
 
