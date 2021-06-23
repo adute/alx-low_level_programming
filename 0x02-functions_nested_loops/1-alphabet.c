016ed03810fd7739ab2594f147844c10449054f1
adute <aduti.alemu@gmail.com>
2021-06-23 01:55:15 -0700

    My functions nested loops 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,3 +10,14 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+void print_alphabet(void)
+{
+	char s = 'a';
+
+	while (s <= 'z')
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,18 @@
+#include <unistd>
+/**
+ *function prints alphabet 10 times
+ *
+ * return void
+ */
+void print_alphabet_x10(void)
+{
+	char s = 'a';
+	int i = 0;
+	while (i <=9)
+	{
+		for ()
+		{
+		_putchar(s);
+		}
+	}
+}
