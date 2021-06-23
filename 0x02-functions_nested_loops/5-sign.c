ba9699b191503dfc6808aeba40fa71a4227885a0
adute <aduti.alemu@gmail.com>
2021-06-23 12:53:43 -0700

    My functions nested loops 3

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_sign - function that  prints of the number
+ * @n: variable to be checked
+ *
+ * Return: 1 for positive
+ *  0 for zero
+ * -1 for negatives
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	if (n == 0)
+		_putchar('0');
+	return (0);
+	if (n < 0)
+		_putchar('-');
+	return (-1);
+
+}
