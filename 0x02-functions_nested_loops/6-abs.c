a5b1c92106a7fb2a07cc93d511ef3b7e1d1a5be3
adute <aduti.alemu@gmail.com>
2021-06-23 13:49:02 -0700

    My functions nested loops 4

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - function computes absolute value
+ * @n: for intigers
+ *
+ * Return: absolte value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -1 * n;
+	else
+		n = n;
+	return (0);
+}
