d6e6ca632b2574983ef292e21b26b5fd008e2c4e
adute <aduti.alemu@gmail.com>
2021-06-25 12:50:07 -0700

    My more functions 3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9 excep 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)	
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		else
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
