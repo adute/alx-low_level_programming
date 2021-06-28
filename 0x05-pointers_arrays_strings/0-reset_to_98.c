4c671aabf592cd7c28b3f20f1bc2df9ae0c868b9
adute <aduti.alemu@gmail.com>
2021-06-28 04:03:15 -0700

    Here we go pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that resets any number to 98
+ *@n: variable to reseted
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - function that swaps values of two intigers
+ * @a: intiger 1
+ * @b: intiger 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
