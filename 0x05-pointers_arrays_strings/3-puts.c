4930efefa0046920c61943e8680dd429ddb727db
adute <aduti.alemu@gmail.com>
2021-06-30 09:37:32 -0700

    Adding strlen

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+int _strlen(char *s);
 
 /**
  * _puts - function that prints string followe*d by new line
@@ -11,8 +12,8 @@ void _puts(char *str)
 	int i = 0;
 	int len;
 
-	len = _strlen(str);
-       
+	len =_strlen(str);
+
 	for(i = 0; i <= len; i++)
 	{
 		_putchar(str[i]);
