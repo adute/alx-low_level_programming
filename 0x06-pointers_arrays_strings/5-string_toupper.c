3db33c5453562ff494651a5800715cf83a627a2a
adute <aduti.alemu@gmail.com>
2021-07-02 13:49:02 -0700

    My upper commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - function that changes lower cases to upper
+ *@s: string
+ * Return: upper
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
