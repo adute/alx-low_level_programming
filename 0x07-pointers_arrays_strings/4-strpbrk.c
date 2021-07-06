c37ba0e7b373fb0acf0782534f1f5449cb191910
adute <aduti.alemu@gmail.com>
2021-07-06 13:59:39 -0700

    My last

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,31 @@
 #include "holberton.h"
 
 /**
- *
+ * _strpbrk - function that gets the length of a prefix substring
+   * @s: string
+   * @accept: checking byte
+   *
+   * Return:  accept that matches s
+   */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	if (s[i] == '\0' || accept[j] == '\0')
+		return ('\0');
+	
+}
