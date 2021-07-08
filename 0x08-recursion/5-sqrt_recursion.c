95bbc616bf5539ce5d3d98a44494a840c74ff2bb
adute <aduti.alemu@gmail.com>
2021-07-08 07:30:19 -0700

    My recursion 4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns natural square root of numbers
+ * @n: number
+ *
+ * Return: sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+	int m = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		sqrt = (n / sqrt + sqrt) / 2;
+		return (_sqrt_recursion(n));
+	}
+}
