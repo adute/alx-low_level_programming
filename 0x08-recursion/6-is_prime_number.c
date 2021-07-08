a6890d53f1e81322015b2733376d3216388c5d53
adute <aduti.alemu@gmail.com>
2021-07-08 13:29:23 -0700

    The prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - checkes if the num is prime
+ * @n: intiger to be checked
+ *
+ * Return: 1 if yes
+ * 0: if not
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n <= 1)
+		return (0);
+	else if (n % i != 0)
+		is_prime_number(n % i);
+		return  (1);
+}
