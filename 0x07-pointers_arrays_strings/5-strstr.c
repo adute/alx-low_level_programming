a47f1fef9e6855067484afed0b9a7af18f6c2b4c
adute <aduti.alemu@gmail.com>
2021-07-09 07:51:06 -0700

    Pushing my holberton header

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * ystack: var 1
+ * 2_strstr - function that locates sub string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Task, static libraries.
