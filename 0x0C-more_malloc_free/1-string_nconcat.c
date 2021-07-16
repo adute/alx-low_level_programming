62a1bec8468cfa3213355bc1f1ecf5fd4f75c9b1
adute <aduti.alemu@gmail.com>
2021-07-16 06:59:44 -0700

    Srtcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ncon = malloc(l1 + n + 1);
 	if (ncon == NULL)
 		return (NULL);
-	for (i = 0; i < (l1 + n); i++)
+	for (i = 0; i < l1 ; i++)
 		ncon[i] = s1[i];
 	for (j = 0; s2[i] != '\0' && j < n ; j++)
 		ncon[i] = s2[j];
