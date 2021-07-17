3a174fedd7d7bb10f9f73110927967c5fc57375b
adute <aduti.alemu@gmail.com>
2021-07-16 18:47:08 -0700

    Strcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,20 +11,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = sizeof(s1);
-	unsigned int i, j;
+	unsigned int l1;
+	unsigned int i = 0;
+	unsigned int j;
 	char *ncon;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	ncon = malloc(sizeof(char) * (l1 + n));
+	while (*s != '\0')
+		i++;
+	l1 = i;
+	ncon = malloc(sizeof(char) * (l1 + n + 1));
 	if (ncon == NULL)
 		return (NULL);
-	for (i = 0; i <= l1; i++)
+	for (i = 0; i < l1; i++)
 		ncon[i] = s1[i];
-	for (j = 0; s2[i] != '\0' && j < n ; j++, i++)
+	for (j = 0; s2[i] != '\0' && j < n ; i++,j++)
 		ncon[i] = s2[j];
 	ncon[i] = '\0';
 	return (ncon);
