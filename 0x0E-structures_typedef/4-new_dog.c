1551944a01ab8e9037d6509223d32bb3ebf8bc09
adute <aduti.alemu@gmail.com>
2021-07-19 17:44:40 -0700

    Completed the last

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(name) * sizeof(owner));
+	d = malloc(sizeof(name) + sizeof(owner));
 	if (d == NULL)
 		return NULL;
 	else
