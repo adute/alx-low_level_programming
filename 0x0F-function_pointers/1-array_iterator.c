17ee4f1aaf4a32d02f259cd8140ab1e55e184b5c
adute <aduti.alemu@gmail.com>
2021-07-21 02:36:05 -0700

    All green

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *array_iterator -executes a function given as a param on each array
+ * @array: array
+ * @size: size
+ * @action: function pointer
+ *
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
