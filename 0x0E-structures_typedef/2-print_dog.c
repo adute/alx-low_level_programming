df55c8a701ca3ba93abb329042c095f7c729b3c1
adute <aduti.alemu@gmail.com>
2021-07-20 18:57:57 -0700

    Reprint

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d != NULL)	
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
-	else
+	if ((*d).name  == NULL || (*d).owner == NULL)
 		printf("(nil)");
+	else
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
