172c3c762372b9f8d16ecc2e3a8d6a721f415fb6
adute <aduti.alemu@gmail.com>
2021-07-21 02:05:52 -0700

    All green

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - function that prints a name
  * @name: name of person
- * f: function pointer
+ * @f:  function pointer
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
 	f(name);
 }
