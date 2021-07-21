931e785d4d97ddcab0927114ddb83b759cc6e29e
adute <aduti.alemu@gmail.com>
2021-07-20 19:53:18 -0700

    Renew

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
 		free(d);
-	return (NULL);
-	if (d)
+		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
