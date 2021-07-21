51f0921760f6f4755272e78cbc67e1da8defb3de
adute <aduti.alemu@gmail.com>
2021-07-20 18:43:17 -0700

    Editted!

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) do {(x < 0) ? (-x) : (x)} while (0)
+#define ABS(x) ((x < 0) ? -(x) : x)
 
 #endif /* 3 - FUNCTION_LIKE_MACRO_H */
