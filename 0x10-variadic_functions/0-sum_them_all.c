abd3b7ca54ac92d59d3cf18bc6337fec336e30aa
adute <aduti.alemu@gmail.com>
2021-07-22 17:05:17 -0700

    Fresh start

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - add all of its parameters
+ * @n: first param
+ * @...: variable pparams
+ *
+ * Return: sum 
+ * 0: if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0;
+	va_list num;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(num, int);
+	va_end(num);
+	return (sum);
+}
