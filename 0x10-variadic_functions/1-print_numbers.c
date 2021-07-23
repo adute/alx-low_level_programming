12d83cc023ce8561d04e11cf11c91fd560f727df
adute <aduti.alemu@gmail.com>
2021-07-22 17:26:18 -0700

    Fresh star

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers \n
+ * @separator: string printed between numbers
+ * @n: number of int passed to function
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	int p;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+	va_start(num, int);
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(num, int);
+		printf("%d", p);
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(num);
+	printf('\n');
+
+}
