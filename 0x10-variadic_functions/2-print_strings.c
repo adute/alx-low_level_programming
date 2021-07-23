e219bbea8cf960789d3fe1d9d53e9239058c0ed4
adute <aduti.alemu@gmail.com>
2021-07-22 18:08:53 -0700

    Fresh star

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,30 +4,30 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers \n
+ * print_strings - prints numbers \n
  * @separator: string printed between numbers
  * @n: number of int passed to function
  *
  * Return: nothing
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list num;
-	int p;
+	va_list str;
+	char *p;
 	unsigned int i;
 
 	if (separator == NULL)
 		return;
-	va_start(num, n);
+	va_start(str, n);
 	if (n != 0)
 		for (i = 0; i < n; i++)
 		{
-			p = va_arg(num, int);
-			printf("%d", p);
+			p = va_arg(str, char *);
+			printf("%s", p);
 			if (i != n - 1)
 				printf("%s", separator);
 		}
-	va_end(num);
+	va_end(str);
 	printf("\n");
 
 }
