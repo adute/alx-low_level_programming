dc2834bdcbe36b3ac4a6a3f404a67a17f57a8d0a
adute <aduti.alemu@gmail.com>
2021-07-29 15:31:26 -0700

    Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_first - function that comes before main execute
+ *
+ *Return : nothing
+ */
+void print_first (void)
+{
+	printf("You're beat and yet, you must allow,\nI bore my house upon my back!\n");
+}
