2d827508fdafb421c337d3f77daf2a829bca3806
adute <aduti.alemu@gmail.com>
2021-07-30 13:21:23 -0700

    Addne

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	newE = malloc(sizeof(list_t));
 	newE->str = str1;
 	newE->next = NULL;
-	newE->len = strlen(str1);
 
 	if (str1 == NULL || newE == NULL)
 	{
@@ -35,6 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		tmp = tmp->next;
 	}
 	tmp->next = newE;
+	newE->len = strlen(str1);
 	}
 	return (tmp);
 
