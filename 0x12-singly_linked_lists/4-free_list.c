510b5d1aa9ba7b3f90cd5b3c4b32235f816d8895
adute <aduti.alemu@gmail.com>
2021-07-30 13:53:33 -0700

    Free like bird

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list_t list
+ * @head: header of list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		 head = head->next;
+		 free(tmp);
+	}
+}
