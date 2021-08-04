de1df1466cc4aa64bd8903bb4ca5d2ebe1524d89
adute <aduti.alemu@gmail.com>
2021-08-04 11:35:06 -0700

    Recode

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - gets nth node of listint_t
+ * @head: header
+ * @index: index of the nde
+ *
+ * Return: nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *search = head;
+
+	if (search)
+	{
+		if (count == index)
+		{	
+			return (search);
+		}
+			count++;
+			search = search->next;
+	}
+	return (NULL);
+}
