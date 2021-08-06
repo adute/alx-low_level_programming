50a347d9965ac39919aa178ee6a93c170aa56bf2
adute <aduti.alemu@gmail.com>
2021-08-06 11:48:43 -0700

    Keep going

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_binary - function that prints the binary rep.
+ * @n: number in binary
+ *
+ * Return: NOthing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+
+	for (i = 1 << 10; i > 0; i >>= 1)
+	{
+		if ((n & i) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+	}
+
+}
