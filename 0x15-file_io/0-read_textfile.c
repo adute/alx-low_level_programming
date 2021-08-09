914da5df4a3aade28080a7842f346d2969184856
adute <aduti.alemu@gmail.com>
2021-08-09 11:40:11 -0700

    Starting

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "holberton.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * read_textfile - reads text file and prints it to
+ * POSIX STDOU
+ * @filename: name of file
+ * @letters: number of letters
+ * Return: number of letters
+ * 0: if filename is null,not opened/read/write
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int file = open(filename, O_RDONLY);
+	char *buf = malloc(sizeof(char) * letters);
+	ssize_t rd, cnt;
+
+	if (filename == NULL || file == -1)
+		return (0);
+
+	rd = read(file, buf, letters);
+	cnt = write(STDOUT_FILENO, buf, rd);
+	if (rd == -1 || cnt == -1 || rd != cnt)
+		return (0);
+	return (cnt);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,12 @@
+This directory contains file I/O functions;-
+
+that reads a text file and prints it to the POSIX standard output
+
+that creates file
+
+that appends text at the end of a file
+
+a program that copies the content of a file to another file
+
+and additional advances tasks
+
