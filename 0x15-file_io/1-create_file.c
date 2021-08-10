d7450152efcf3911eed2758e8a35f0d1c4827218
adute <aduti.alemu@gmail.com>
2021-08-09 21:33:29 -0700

    Come again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file = open(filename, O_CREAT | O_RDWR | O_TRUNC,
+	int file = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
 			600);
 	size_t buf = (size_t)malloc(sizeof(text_content));
 	int crt;
