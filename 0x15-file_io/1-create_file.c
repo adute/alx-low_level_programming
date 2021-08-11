d014f87a87647138b1eef91436303ca4692a4a3b
adute <aduti.alemu@gmail.com>
2021-08-11 12:04:13 -0700

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		free(text_context);
+		free(text_content);
 		return (-1);
 	}
 	crt = write(file, text_content, sizeof(text_content) - 1);
