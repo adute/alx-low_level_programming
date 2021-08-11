47b430e9083dd626b2dd0004974ec9c989f1e289
adute <aduti.alemu@gmail.com>
2021-08-11 05:27:29 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,16 +18,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int crt;
 	int file = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
 			600);
-	/*size_t *buf = (size_t *)malloc(sizeof(text_content));*/
-	int crt;
+	text_content = malloc(sizeof(text_content));
 
-	if (filename == NULL || file == -1 ||
-			text_content == NULL)
+	if (filename == NULL || file == -1 )
 		return (-1);
-
-	crt = write(STDOUT_FILENO, text_content, sizeof(char));
+	if (text_content == NULL)
+	{
+		free(text_content);
+		return (-1);
+	}
+	crt = write(STDOUT_FILENO, text_content, sizeof(text_content));
 	if (crt == -1)
 	{
 	/*	free(buf);*/
