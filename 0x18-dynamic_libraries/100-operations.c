307d71d76fa2278065c94ccce9002ab26e823045
adute <aduti.alemu@gmail.com>
2021-09-24 07:20:45 -0700

    AM  back

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * add - function that adds two numbers
+ * @a: first num
+ * @b: second num
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that subtract
+ * @a: first num
+ * @b: second num
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiply two numbers
+ * @a: first num
+ * @b: second num
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that divides two numbers
+ * @a: first num
+ * @b: second num
+ * Return: the quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that modulo two numbers
+ * @a: first num
+ * @b: second num
+ * Return: eqv.class
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
