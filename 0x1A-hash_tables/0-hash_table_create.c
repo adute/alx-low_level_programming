a554f8a4f475ebbc3713a01962fbe3dad1640308
adute <aduti.alemu@gmail.com>
2021-10-13 11:38:17 -0700

    Hash 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,12 +14,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size <= 0)
 		return (NULL);
-	
+
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_node_t*) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
