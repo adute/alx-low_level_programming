efe698b982c4f9c686f1529aeda196dbf2bd943a
adute <aduti.alemu@gmail.com>
2021-10-13 11:29:36 -0700

    Hash 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: table
+ * if fails: NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	if (size <= 0)
+		return (NULL);
+	
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_node_t*) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	table->size = size;
+
+	return (table);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives the index of the key
+ * @key - the key
+ * @size - the size of the array of the hash table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
