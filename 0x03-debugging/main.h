78d723129aa5953a58c3625c117b82b1a9da98aa
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-25 13:14:16 -0700

    Check the largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,16 @@
 #define MAIN_H
 
 #include <stdio.h>
+/**
+ * positive_or_negative - check number if its positive or not
+ * @i: number to be checked
+ *
+ * largest_number - checks for the largest number from the three
+ * @a: number to be compared
+ * @b: number to be compared
+ * @c: number to be compared
+ */
 
 void positive_or_negative(int i);
-
+int largest_number (int a, int b, int c);
 #endif /* MAIN_H */
