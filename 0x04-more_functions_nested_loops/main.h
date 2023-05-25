3c7d807378a603dd0de2da1f0f82cacc1c915dad
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-25 10:57:01 -0700

    Check for digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,8 +3,10 @@
 
 /**
  * _isupper - function checks for upper case
+ * _isdigit - function checks digit (0-9)
  * @c: character to be checked
  * Return: 1 if yes / 0
  */
 int _isupper(int c);
+int _isdigit(int c);
 #endif
