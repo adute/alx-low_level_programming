9e8f583e1c6564f55f19197636eeb442f2bba2d0
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-25 12:50:57 -0700

    Test function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -tests function that check positive or negative of anumber
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
