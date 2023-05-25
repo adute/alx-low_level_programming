929c7b5493767f132f59da5ff77187fc30965d84
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-25 11:06:01 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two numbers
+ * @a: multiple 1
+ * @b: multiple 2
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,13 @@
  * _isdigit - function checks digit (0-9)
  * @c: character to be checked
  * Return: 1 if yes / 0
+ *
+ * mul - function that multiplies two numbers
+ * @a: number 1
+ * @b: multiple 2
+ * Return: multiple of a and b
  */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
