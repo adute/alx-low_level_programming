bba1946723d1daef9c0f064a02224963d61cf923
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-26 17:18:29 -0700

    Print intigers with _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,9 +17,14 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	else if (n > 0 )
+	else if (n > 0 && n < 100)
 	{
 		_putchar (n / 10 + '0');
+		_putchar (n % 10 + '0');
+	}
+	else if (n > 100 && n < 1000)
+	{
+		_putchar (n / 100 + '0');
 		_putchar (n % 10 + '0');
 	}
 	_putchar('\n');
