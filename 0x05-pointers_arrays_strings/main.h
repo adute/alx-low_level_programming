ff23b7fa7211c4eb0519fd01f77da5c4c74092d1
Adanech Alemu <aduti.alemu@gmail.com>
2023-05-29 13:52:32 -0700

    Swap two pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,10 @@
  * reset_to_98 - resets integer to 98
  * @n: integer to be reseted
  *
+ * swap_int - swaps the value of two integers
+ * @a: integer one
+ * @b: integer 2
  */
 void reset_to_98(int *n);
-
-#endif
+void swap_int(int *a, int *b);
+#endif /* main.h */
