79734b382fdb56002609142f11e0fbc8ad13693d
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-05 17:48:45 -0700

    Searching for a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: pointer string
+ * @c: char to found
+ * Return: pointer first occurance of c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+	if (s[i] == c)
+	{
+	i++;
+	s++;
+	}
+	return (s);
+	}	
+
+	return NULL;
+	
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,8 +10,12 @@
  * _memcpy -  copies memory area.
  * @dest: destination 
  * @src: source
+ *
+ * _strchr -  locates a character in a string
+ * @c: character to be searched
  */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
