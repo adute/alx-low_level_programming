0c6fc50b33ce0d342b68d3b6caeda644dc898ffd
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-06 01:39:50 -0700

    Print string Reversed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * _strlen - print length of the given string
+ * @s: string whose length to be measured
+ * Return: the length of string
+ *
+ */
+int _strlen(char *s)
+{
+        int len = 0;
+
+        while (*s != '\0')
+        {
+                len++;
+                s++;
+        }
+
+        return (len);
+}
+
+/**
  * print_rev - print a string in reverse order
  * @s: string to be printed reversed.
  *
