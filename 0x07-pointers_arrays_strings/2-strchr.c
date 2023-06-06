eba1501dc7b847243cc0dc68f0ce8edf97ffc720
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-05 18:04:17 -0700

    Searching for a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 	if (s[i] == c)
 	{
@@ -18,8 +18,7 @@ char *_strchr(char *s, char c)
 	s++;
 	}
 	return (s);
-	}	
+	}
+	return (NULL);
 
-	return NULL;
-	
 }
