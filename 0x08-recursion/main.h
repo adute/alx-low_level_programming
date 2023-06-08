ceb9470fe81f9f72262533079c5862387a6d4861
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-08 07:25:52 -0700

    Print reversed string using recursive function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,10 @@
  *
  * _puts_recursion - prints string
  * @s: the string
+ *
+ * _print_rev_recursion - prints string reversed recursively
  */
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
