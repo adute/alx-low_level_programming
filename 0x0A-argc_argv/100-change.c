63d4003e8b6c04c716f3d1c74d52472c4ee2a110
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-09 14:09:15 -0700

    Printing minimum number of coins of change of maoney

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  program prints minimum number of coins to make
+ * @argc: argumrnt count
+ * @argv: argumrnt vector
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int cent[5] = {25, 10, 5, 2, 1};
+	int change;
+	int i = 0;
+
+	if (argc > 1)
+	{
+	while (i < 5)
+	{
+	if (atoi(argv[1]) < cent[i])
+		i++;
+	else if (atoi(argv[1]) == cent [i])
+		change = atoi(argv[1]) / cent[i];
+	}
+	printf("%d\n", change);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	exit(EXIT_SUCCESS);
+}
