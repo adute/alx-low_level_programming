946d81bd7620ff63d19a81709a7fc39dd57f788e
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-12 11:46:35 -0700

    Home made strdup function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array -a function that returns a pointer to a newly allocated
- * space in memory, which contains a copy of the string 
+ * _strdup -a function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
  * given as a parameter.
  * @str: the string
  * Return: pointer to the duplicated string on SUCCESS
