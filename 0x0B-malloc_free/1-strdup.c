2878ba6021ff8728811bcf9c12ac013c2c7b72f6
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-13 11:58:33 -0700

    Home made strdup function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _strlen - print length of the given string
+ * @s: string whose length to be measured
+ * Return: the length of string
+ *
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}/**
  * _strdup -a function that returns a pointer to a newly allocated
  * space in memory, which contains a copy of the string
  * given as a parameter.
@@ -11,9 +28,11 @@
 char *_strdup(char *str)
 {
 	char *p;
+	int len;
 	int i = 0;
 
-	p = malloc(sizeof(str) + 1);
+	len = _strlen(str);
+	p = malloc(len * sizeof(char));
 
 	if (p == NULL || str == NULL)
 	{
