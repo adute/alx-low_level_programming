824d8d435d9f1f2869e7806075a426de3b4b9cf7
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-15 12:32:59 -0700

    Adding Readme and function malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked -  a function that allocates memory using malloc
+ * @b: size of memory to be allocated
+ * Return: pointer to the allocated memory
+ * if malloc fails, the malloc_checked function should 
+ * cause normal process termination with a status value of 98
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
