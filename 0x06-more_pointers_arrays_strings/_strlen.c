ea7dd89e55ec781378573029bd9d1c0fbb14bc43
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-14 17:28:25 -0700

    Readme and string concat function

---
+++ 0x06-more_pointers_arrays_strings/_strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - print length of the given string
+ * @s: string whose length to be measured
+ * Return: the length of string
+ *
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
