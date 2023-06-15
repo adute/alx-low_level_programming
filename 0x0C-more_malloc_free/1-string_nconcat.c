08558af3cb7bfd4a8c7d3dad7e23c065ad8c4d36
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-15 13:35:34 -0700

    strncat using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,33 +29,35 @@ unsigned int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0;
-	unsigned int len1, len2;
+	unsigned int len1;
 	char *ptr;
 
 	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	/*len2 = _strlen(s2);*/
 
-	if (n >= len2)
-	{
-		ptr = malloc(len1 + len2 + 1);
-	}
-	else
-	{
 	ptr = malloc(len1 + n + 1);
-	}
+	
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	while (i < n)
-	{
-		ptr = s1;
-		s1[i] = s2[i];
-		i++;
-	}	
 	ptr = s1;
-	ptr[i] = '\0';
+	while (i < len1)
+	{
+		ptr++;
+		i++;
+	}
 
-	return (ptr);
+	while (n--)
+	{
+		if (!(*ptr++ = *s2++))
+		{
+			return (s1);
+		}
+
+	}
+	*ptr = '\0';
+
+	return (s1);
 }
 
