4c5aa827c43b615d50dc0b507ff83e3d4a926929
Adanech Alemu <aduti.alemu@gmail.com>
2023-06-25 00:51:44 -0700

    fix error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (1);
-	if (array == NULL || cmp == NULL)
-		return (0);
+	if (array == NULL || cmp == NULL || size == 0)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]))
 			return (i);
 		i++;
 	}
-	return (-1);
+	return (0);
 
 }
