bc8ebc9885e689d467d587a3cc54103512cdb405
Adanech <aduti.alemu@gmail.com>
2023-06-27 04:54:16 -0700

    Fix compilation error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,33 @@
 #include "main.h"
 /**
- * _print_rev_recursion - print a string in reverse order
- * @s: string to be printed reversed.
+ * _strlen_recursion - print length of the given string
+ * @s: string whose length to be measured
+ * Return: the length of string
  *
  */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+/**
+  * _print_rev_recursion - print a string reversed, recursively
+  * @s: the string
+  *
+  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != 0)
+	int len;
+
+	len = _strlen_recursion(s);
+	 if (len != 0)
 	{
-		_putchar(s[i]);
-		--i;
-		--s;
-		_print_rev_recursion(s );
+		_putchar(s[len]);
+		len--;
+		_print_rev_recursion(s);
 	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - finds the square root of number
+ * @n: the number
+ * Return: sqrt or -1 ifnot
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+
+	return (sqrt);
+}
