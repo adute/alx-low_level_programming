83377d2427d43084d2aa4631e2543ad544404e24
Adanech <aduti.alemu@gmail.com>
2023-07-04 14:16:24 -0700

    Free list and seat head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a list and setts head to NULL
+ * @head: head of a lest
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while ((*head)->next != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
