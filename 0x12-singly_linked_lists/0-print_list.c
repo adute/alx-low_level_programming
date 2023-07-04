1f3d87d7995d8b2bb5d82d043418989e40d98ffc
Adanech <aduti.alemu@gmail.com>
2023-07-04 15:06:51 -0700

    Print string list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * size_t - function that prints all the elements of a list_t list
+ * print_list - function that prints all the elements of a list_t list
  * @h: pointer
  * Return: the number of nodes
  * if str is NULL, print [0] (nil)
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		count++;
+	count++;
 	if (h->str == NULL)
 	{
 	printf("[%d] (nil)\n", 0);
