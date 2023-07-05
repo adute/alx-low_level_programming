87b8364720ff160ee7e3c2aa82bb3896b0bbd69c
Adanech <aduti.alemu@gmail.com>
2023-07-04 23:11:16 -0700

    Freeing a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,9 +12,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (head == NULL)
+	if (head == NULL || new == NULL)
 	{
-	return (NULL);	
+	return (NULL);
 	}
 	new->n = n;
 	new->next = (*head);
