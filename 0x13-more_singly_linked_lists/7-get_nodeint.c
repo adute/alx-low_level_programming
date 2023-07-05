69a77e8c25ae0fbec09c383f630da2e623e8cf0e
Adanech <aduti.alemu@gmail.com>
2023-07-05 13:40:24 -0700

    Get the nth node of a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,9 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *tmp;
+	listint_t *tmp = malloc(sizeof(listint_t));;
 
-	if (head == NULL)
+	if (head == NULL || tmp == NULL)
 	{
 		return (NULL);
 	}
