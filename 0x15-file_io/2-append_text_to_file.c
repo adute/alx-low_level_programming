638bdb2671d10b20aa962ee613e148058344628e
Adanech <aduti.alemu@gmail.com>
2023-07-11 14:34:23 -0700

    Append to textfile

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -18,18 +19,19 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	mode_t mode = S_IRUSR | S_IWUSR;
 
-	fd = open(filename, O_APPEND, mode);
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_RDWR | O_APPEND, 664);
+	if (fd == -1)
 	{
 	return (-1);
 	}
-	else
-	{
-	write(fd, text_content, sizeof(text_content) - 1);
+	
+	write(fd, text_content, strlen(text_content));
 	close(fd);
-	}
 	return (1);
 
 }
